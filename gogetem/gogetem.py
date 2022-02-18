e3bf06090f0f12d8d38768b2cc43164bd0bb227b
Dillon Barker <dor.barker@gmail.com>
2022-02-18 17:51:19 -0600

    add basic querying

--- gogetem/gogetem.py
+++ gogetem/gogetem.py
@@ -1,4 +1,71 @@
 import argparse
 
+# from SPARQLWrapper import SPARQLWrapper, JSON
 
-from SPARQLWrapper import SPARQLWrapper, JSON
+
+def arguments():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--go-terms", nargs="+", type=int, help="GO terms to search for"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+
+    args = arguments()
+
+
+def query_prefix() -> str:
+
+    prefixen = [
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+        "PREFIX up: <http://purl.uniprot.org/core/>",
+        "PREFIX go: <http://purl.obolibrary.org/obo/GO_>",
+    ]
+
+    return "\n".join(prefixen)
+
+
+def query_select(include_amino_acids: bool = False) -> str:
+
+    aa_var = "?aa_sequence" if include_amino_acids else ""
+    select = f"SELECT ?protein ?name ?link {aa_var}"
+    return select
+
+
+def query_match(go_terms: list[int], include_amino_acids: bool = False) -> str:
+
+    go_terms_str = " ".join(["{"] + [f"go:{g}" for g in go_terms] + ["}"])
+
+    get_aa_sequence = """
+        ?protein up:sequence ?seq .
+        ?seq rdf:value ?aa_sequence . 
+    """
+
+    aa_sequence_query = get_aa_sequence if include_amino_acids else ""
+
+    where = [
+        "WHERE {",
+        f"values ?go_terms {go_terms_str}",
+        "?protein a up:Protein ;",
+        "    rdfs:label ?name ;",
+        "    up:classifiedWith|(up:classifiedWith/rdfs:subClassOf) ?go_terms .",
+        aa_sequence_query,
+        "?protein rdfs:seeAlso ?link .",
+        "?link up:database ?database .",
+        "?database rdfs:label 'EMBL nucleotide sequence database' . ",
+        "}",
+    ]
+
+    return "\n".join(where)
+
+
+if __name__ == "__main__":
+    main()
